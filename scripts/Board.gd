3230fac3fca5429489422a6b115f2e2051ed6445
Ivy Case <case@berkeley.edu>
2023-07-07 20:53:34 -0700

    Basic Check detection!

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -13,6 +13,8 @@ var current_piece_moves = []
 ### PRIVATE FUNCTIONS ###
 
 func _ready():
+	Global.current_board = self
+	
 	var board_row = []
 	var destroyed_row = []
 	for i in board_size.x: 
@@ -26,13 +28,13 @@ func _ready():
 	add_piece(Global.Type.PAWN, Global.Team.GREEN, Vector2i(2, 0))
 	add_piece(Global.Type.KING, Global.Team.PINK, Vector2i(0, 3))
 	add_piece(Global.Type.PAWN, Global.Team.PINK, Vector2i(1, 2))
-	print("current board: ", board_matrix)
+	#print("current board: ", board_matrix)
 		
 func _input(_event):
 	if Input.is_action_just_released("click"):
 		var mouse_pos = get_local_mouse_position()
 		var coords = Tilemap.local_to_map(mouse_pos)
-		print("clicked tile: ", coords)
+		#print("clicked tile: ", coords)
 		
 		if !(0 <= coords.x && coords.x < board_size.x) || !(0 <= coords.y && coords.y < board_size.y): # not inside board
 			current_piece = null
@@ -47,8 +49,8 @@ func _input(_event):
 		else:
 			piece_select(board_matrix[coords.x][coords.y])
 			set_highlight(true)
-			print("current_piece: ", current_piece)
-			print("current_piece moves: ", current_piece_moves)
+			#print("current_piece: ", current_piece)
+			#print("current_piece moves: ", current_piece_moves)
 			
 ### PUBLIC FUNCTIONS ###
 			
@@ -65,8 +67,11 @@ func remove_piece(tile):
 	var piece = board_matrix[tile.x][tile.y]
 	if piece == null: return
 	
+	#print(piece, " removed")
+	
 	board_matrix[tile.x][tile.y] = null
 	remove_child(piece)
+	Global.remove_piece(piece, piece.team)
 	piece.queue_free()
 	
 func destroy_tile(tile):
@@ -74,15 +79,16 @@ func destroy_tile(tile):
 	destroyed_matrix[tile.x][tile.y] = true
 	Tilemap.set_cell(1, tile, 0, Vector2i(0, 14))
 			
-func move_current_piece(tile):
-	if tile in current_piece_moves:
-		current_piece_moves.erase(tile)
+func move_current_piece(move):
+	if move in current_piece_moves:
+		current_piece_moves.erase(move)
 		for unused in current_piece_moves:
 			destroy_tile(unused)
 		
-		remove_piece(tile)
-		board_matrix[tile.x][tile.y] = current_piece
-		current_piece.set_tile(tile, Tilemap.map_to_local(tile))
+		remove_piece(move)
+		board_matrix[current_piece.tile.x][current_piece.tile.y] = null
+		board_matrix[move.x][move.y] = current_piece
+		current_piece.set_tile(move, Tilemap.map_to_local(move))
 	
 		Global.switch_teams()
 		
@@ -91,7 +97,7 @@ func piece_select(piece):
 	if piece.team != Global.get_current_team(): return piece_deselect()
 	
 	current_piece = piece
-	current_piece_moves = legal_moves(piece)
+	current_piece_moves = filter_in_check_moves(piece, get_legal_moves(piece))
 	
 func piece_deselect():
 	current_piece_moves = []
@@ -104,17 +110,44 @@ func set_highlight(do_highlight):
 	
 ### GET LEGAL MOVES ###
 
-func filter_invalid_moves(piece, moves):
+func in_check(team, matrix=board_matrix, destroyed=destroyed_matrix):
+	for move in Global.get_all_legal_moves(Global.get_next_team(team), matrix, destroyed):
+		#print("checked move: ", move)
+		var piece = matrix[move.x][move.y]
+		if piece != null && piece.type == Global.Type.KING && piece.team == team: return true
+	return false
+
+func filter_in_check_moves(piece, moves):
+	var filter = moves.duplicate()
+	
+	for move in moves:
+		var test_destroyed = destroyed_matrix.duplicate(true)
+		for other_move in moves:
+			if other_move != move: 
+				test_destroyed[other_move.x][other_move.y] = true
+		
+		var test_matrix = board_matrix.duplicate(true)
+		test_matrix[piece.tile.x][piece.tile.y] = null
+		test_matrix[move.x][move.y] = piece
+		
+		if in_check(piece.team, test_matrix, test_destroyed):
+			#print("filtered: ", move)
+			filter.erase(move)
+		
+	#print("second filter: ", moves)
+	return filter
+
+func filter_invalid_moves(piece, moves, matrix=board_matrix, destroyed=destroyed_matrix):
 	var filter = moves.duplicate()
 	
 	for move in moves:
 		if !(0 <= move.x && move.x < board_size.x) || !(0 <= move.y && move.y < board_size.y): filter.erase(move) # out of board
-		elif board_matrix[move.x][move.y] != null && board_matrix[move.x][move.y].team == piece.team: filter.erase(move) # occupied by friendly piece
-		elif destroyed_matrix[move.x][move.y]: filter.erase(move) #tile is destroyed
+		elif matrix[move.x][move.y] != null && matrix[move.x][move.y].team == piece.team: filter.erase(move) # occupied by friendly piece
+		elif destroyed[move.x][move.y]: filter.erase(move) #tile is destroyed
 	
 	return filter
 	
-func get_pawn_captures(piece):
+func get_pawn_valid_moves(piece, matrix=board_matrix, destroyed=destroyed_matrix):
 	var captures
 	match piece.team:
 		Global.Team.GREEN:
@@ -125,19 +158,27 @@ func get_pawn_captures(piece):
 	
 	var filter = captures.duplicate()
 	for move in captures:
-		if board_matrix[move.x][move.y] == null || board_matrix[move.x][move.y].team == piece.team: filter.erase(move)
+		if matrix[move.x][move.y] == null || matrix[move.x][move.y].team == piece.team: filter.erase(move)
+	
+	var moves = []
+	for move in piece.get_pattern():
+		moves.append(move + piece.tile)
+		
+	filter.append_array(moves)
+	for move in moves:
+		if !(0 <= move.x && move.x < board_size.x) || !(0 <= move.y && move.y < board_size.y): filter.erase(move) # out of board
+		elif matrix[move.x][move.y] != null: filter.erase(move) # occupied by piece
+		elif destroyed[move.x][move.y]: filter.erase(move) #tile is destroyed
 	
 	return filter
 
-func legal_moves(piece):
+func get_legal_moves(piece, matrix=board_matrix, destroyed=destroyed_matrix):
 	var moves = []
 	if piece.type == Global.Type.PAWN:
-		moves.append_array(get_pawn_captures(piece))
+		return get_pawn_valid_moves(piece, matrix)
 	
 	for move in piece.get_pattern():
 		moves.append(move + piece.tile)
-	
-	print(moves)
 		
-	return filter_invalid_moves(piece, moves)
+	return filter_invalid_moves(piece, moves, matrix, destroyed)
 	
--- scripts/Global.gd
+++ scripts/Global.gd
@@ -3,6 +3,7 @@ extends Node
 enum Type {PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING}
 enum Team {GREEN, PINK}
 
+var current_board = null
 var current_team = Team.PINK
 var green_pieces = []
 var pink_pieces = []
@@ -23,15 +24,36 @@ func remove_piece(piece, team):
 	check_win()
 
 func check_win():
-	if len(green_pieces == 1) && len(pink_pieces == 1):
+	if len(green_pieces) == 1 && len(pink_pieces) == 1:
 		print("win!")
 
 func switch_teams():
-	match current_team:
-		Team.GREEN:
-			current_team = Team.PINK
-		Team.PINK:
-			current_team = Team.GREEN
+	current_team = get_next_team(current_team)
 
 func get_current_team():
 	return current_team
+	
+func get_next_team(team):
+	match team:
+		Team.GREEN:
+			return Team.PINK
+		Team.PINK:
+			return Team.GREEN
+
+func get_all_legal_moves(team, matrix=current_board.board_matrix, destroyed=current_board.destroyed_matrix):
+	var moves = []
+	var pieces
+	
+	match team:
+		Team.GREEN:
+			pieces = green_pieces
+		Team.PINK:
+			pieces = pink_pieces
+			
+	for piece in pieces:
+		moves.append_array(current_board.get_legal_moves(piece, matrix, destroyed))
+		
+	print("all enemy legal moves: ", moves)
+	return moves
+	
+	
